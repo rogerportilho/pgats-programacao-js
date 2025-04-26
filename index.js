a1b737e9aaf7ab77d24ca839d1172600cc3ff18b
Roger PG <rogerportilhoart@gmail.com>
2025-04-26 16:50:07 -0300

    Resolvendo conflito index

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Console API
  */
@@ -45,52 +44,4 @@ let tamanho = 'M'
 var irmaos
 
 console.log(irmaos)
-=======
-/**
- * Console API
- */
-
-console.log("Aula inicial JS")
-console.error("Falha na execução")
-console.warn("Atenção")
-
-console.table([{
-  nome: 'Samuel',
-  turma: '02',
-  disciplina: 'Prog JS'
-},
-{
-  nome: 'Lucas',
-  turma: '02',
-  disciplina: 'Prog JS'
-}])
-
-/**
- * Constantes e Variáveis
- */
-
-// const - para informações que nunca mudam
-// var ou let - para informações que podem mudar
-
-// informacoes de um dog que nao mudam
-const nome = 'Loki'
-const raca = 'SRD / vira lata'
-const sexo = 'Macho'
-const cor = 'Caramelo'
-const dataDeNascimento = '01/01/2020'
-const porte = 'M'
-
-// informacoes que mudam
-let idade = 5
-let peso = 10.5
-let vacinado = true
-let castrado = false
-let tamanho = 'M'
-
-// hosting -> içamento ou elevação
-
-var irmaos
-
-console.log(irmaos)
->>>>>>> 74326a0a2355f6cc3bfe841745f5bc062cedd69e
 irmaos = ["Thor", "Hela"]
