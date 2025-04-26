74326a0a2355f6cc3bfe841745f5bc062cedd69e
Rogerio Portilho Gonçalves <rogerioportillo@gmail.com>
2025-04-26 12:16:55 -0300

    Add files via upload

---
+++ 02-exercicio-v2.js
@@ -0,0 +1,40 @@
+/**Exercício Individual
+Programação para Automação de Testes
+Hands-on
+Validador de idade mínima para adoção
+Crie um script que verifique se um dog pode ser adotado com base na idade mínima 
+definida, por exemplo, 2 anos.
+Use os operadores adequados e exiba:
+Nome do dog
+Idade
+Se está apto ou não para adoção
+Extra: aplique uma regra com operador lógico para permitir que se o cão 
+for de pequeno porte, pode ser adotado independente da idade.
+ */
+
+const nome = 'Ada'
+const idade = 0
+const porte = 'P'
+
+const idadeMinima = 2
+
+function verificarSePodeSerAdotado(idade, porte) {
+  const adocao = idade >= idadeMinima 
+                ? 'sim'
+                : porte === 'P' 
+                ? 'sim'
+                : 'não'
+
+  // return adocao
+  console.log(adocao)
+}
+
+// idade 1 + porte M = nao
+// idade 2 + porte M = sim, pela idade
+// idade 2 + porte P = sim, pela idade
+// idade 1 + porte P = sim, pelo porte
+
+verificarSePodeSerAdotado(1, 'M')
+verificarSePodeSerAdotado(2, 'M')
+verificarSePodeSerAdotado(2, 'P')
+verificarSePodeSerAdotado(1, 'P')
